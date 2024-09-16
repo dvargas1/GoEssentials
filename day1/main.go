39106475e548ddc4baba924523e8350c23ce3825
dvargas1 <daniel_v93@hotmail.com>
2024-09-16 00:26:50 -0300

    day1

---
+++ day1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Esse é meu primeiro print em Go :)	
+	fmt.Println("Olá, Mundo!")
+
+	// Conseguimos printar números diretamente	
+	fmt.Println(42)
+
+	// ou expressões
+	fmt.Println(40 + 2)
+
+	fmt.Println("Daniel está aprendendo a programar em Go?")
+	fmt.Println(true)
+}
